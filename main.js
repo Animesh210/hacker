6ce805ffdee8aaa3bc32c076f5401a62fe130737
Animesh210 <125130735+Animesh210@users.noreply.github.com>
2023-03-16 17:05:10 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+function done() {
+   window.alert("You Have been HACKED"); 
+   document.getElementById("h2").style.display="table-cell"; 
+   document.getElementById("btn").style.display="none";
+   document.getElementById("input").style.display="none";   
+    }
+
+    function danger() {
+window.alert("Windows System: Sorry :( There is nothing we can do about it");
+document.getElementById("btn2").style.display="table";
+document.getElementById("btn1").style.display="none";
+    }
+
+    function a() {
+        window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+        document.getElementById("btn2").style.display="none";
+        document.getElementById("p").style.display="table"
+        document.getElementById("btn3").style.display="table";
+    }
+
+    function redo(){
+        document.getElementById("btn").style.display="table";
+        document.getElementById("btn1").style.display="table";        
+        document.getElementById("p").style.display="none";
+        document.getElementById("h2").style.display="none";
+        document.getElementById("input").style.display="table";        
+        document.getElementById("btn3").style.display="none";
+        document.getElementById("img").style.display="table";       
+    }  
+
+    
+
